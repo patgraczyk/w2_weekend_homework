a79a2b003244424df5c461cc44254483d2166c78
Pat <graczyk.pat@gmail.com>
2018-07-08 11:07:33 +0100

    song added

--- room.rb
+++ room.rb
@@ -1,25 +1,26 @@
 class Room
 
-attr_reader :price,
+attr_reader :name, :price, :songs_in_room
 
-def initialize(price, number_of_guests)
+def initialize(price, name)
   @price = price
+  @name = name
   @number_of_guests = []
-  @songs_in_room= songlist
+  @songs_in_room = []
 end
 
 def add_song_to_room(song)
   @songs_in_room << song
 end
 
-def check_in_guest_room(guest)
-  @number_of_guests << guest
-end
-
-def check_out_guest_room(guest)
-  index = @number_of_guests.index(person)
-  @number_of_guests.slice!(index, 1)
-end
+# def check_in_guest_room(guest)
+#   @number_of_guests << guest
+# end
+#
+# def check_out_guest_room(guest)
+#   index = @number_of_guests.index(person)
+#   @number_of_guests.slice!(index, 1)
+# end
 
 
 
--- specs/room_specs.rb
+++ specs/room_specs.rb
@@ -8,6 +8,7 @@ require_relative('../guest.rb')
 class RoomTest < MiniTest::Test
 
 def setup
+  @room1 = Room.new("Disney Room", 5)
   @song1 = Song.new("Under the sea")
   @song2 = Song.new("Be a Man")
   @song3 = Song.new("How far I'll go")
@@ -15,17 +16,18 @@ def setup
   @guest1 = Guest.new("Walt Disney")
 end
 
-def test_song_in_roon
-  assert_equal(1, @songs_in_room.length)
+def test_add_song_to_room
+  @room1.add_song_to_room(@song1)
+  assert_equal(1, @room1.songs_in_room.length)
 end
 
-def test_checkin_guest_from_room
-  assert_equal(1, @number_of_guests.length)
-end
-#
-def test_checkout_guest_from_room
-  assert_equal(0, @number_of_guests.length)
-end
+# def test_checkin_guest_from_room
+#   assert_equal(1, @number_of_guests.length)
+# end
+# #
+# def test_checkout_guest_from_room
+#   assert_equal(0, @number_of_guests.length)
+# end
 
 
 
